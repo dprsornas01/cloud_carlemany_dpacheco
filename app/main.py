ab729e7ccde27ba32c3e8f1a61beff92f09421c7
dprsornas01 <92050966+dprsornas01@users.noreply.github.com>
2024-04-26 23:34:32 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/healthcheck")
 async def healthcheck() -> dict[str, str]:
     return {"status": "ok"}
+
+@app.post("/test/{id}")
+async def test(id: int) -> dict[str, str]:
+        return {"test": id}
